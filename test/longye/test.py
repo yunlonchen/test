6ecb480815306748c84e80b863bef5b88bde8db4
yunlonchen <342527522@qq.com>
2015-02-01 15:32:42 +0800

    test

---
+++ test/longye/test.py
@@ -0,0 +1,146 @@
+#counter = 0
+#while counter < 3:
+#    print 'loop #%d' % (counter)
+ #   counter += 1
+    
+#squared = [x ** 2 for x in range(5) if not x % 2]
+#for i in squared:
+#    print i
+    
+
+#filename = raw_input('enter file name:')
+#fobj = open(filename,'r')
+#for eachLine in fobj:
+  #  print eachLine,
+   # fobj.close()
+
+    
+#def addme2me(x):
+#    'apply + operation to argument'
+ #   return (x + x)
+
+
+
+#print addme2me(2)
+
+
+
+#test ='123'
+#@print test
+
+
+
+
+
+
+
+#!/usr/bin/env python
+1 + 2 * 4
+
+#print 1 + 2
+
+#print 1 * 2
+
+#print 1 + 2 **3
+
+#print 6 / 2
+
+#print (8 % 5)**2
+
+#bitch = raw_input('what is your nanme')
+#print 'you ge doubi jiao',bitch
+
+#num = raw_input('qianwanzhicaonima')
+#print 'you ge sha bi have %d' % int(num)
+
+#counter = 0
+#while counter < 11:
+#    print 'num %d' % (counter)
+#    counter = counter + 1
+ 
+#x = raw_input('numerber')
+#if x > 0:
+ #   print '>0'
+#else:
+ #   if x <0:
+ #       print'<0'
+  #  else:
+  #      print'=0'
+
+
+#m = 12
+#m %= 7
+#print m
+
+#m **= 2
+#print m
+
+#x = 'a'
+#y = x
+#print y
+#!/usr/bin/python
+ 
+#f = open('C:\\Users\\v_ylchen\\Desktop\\caonima.txt','w')
+
+
+#counter = raw_input('give me a number:')
+#while int(counter) < 3:
+ #   print 'number #%d' % (int(counter))
+    
+
+
+#number = raw_input('shurushuzi:')
+#if (int(number) > 0 and int(number) < 100):
+#    print 'true'
+#else:
+#    print 'false'
+
+
+#list = [1,2,3,4,5]
+#print reduce(lambda x,y:x+y,list)/len(list)
+
+
+#for item in ['abc','def','ghi']:
+ #   print item
+
+
+
+#"""list = [1,2,3,4,5]
+
+#a = raw_input('shurushuzi1:')
+#b = raw_input('shurushuzi2:')
+#c = raw_input('shurushuzi3:')
+#d = raw_input('shurushuzi4:')
+#e = raw_input('shurushuzi5:')
+
+#list[0] = int(a)
+#list[1] = int(b)
+#list[2] = int(c)
+#list[3] = int(d)
+#list[4] = int(e)
+
+#number = 0
+
+#for item in list:
+#    number = item + number
+    
+#print number"""
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+     
+
+
+
+
+
